6acf418d9978b20e73dc7456be718f2697343e55
Abdullah Raihan Shamil <raihanshamil33@gmail.com0>
2023-11-30 10:48:59 +0600

    cat added

--- src/pages/home/SpecialDishes.jsx
+++ src/pages/home/SpecialDishes.jsx
@@ -1,11 +1,42 @@
+/* eslint-disable react/prop-types */
 import React, { useEffect, useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { FaHeart} from "react-icons/fa"
+import { FaAngleRight, FaAngleLeft  } from "react-icons/fa6";
+import Cards from "./Cards";
+
+const SampleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    >
+      NEXT
+    </div>
+  );
+};
+
+const SamplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    >
+      BACK
+    </div>
+  );
+};
 
 const SpecialDishes = () => {
   const [recipes, setRecipes] = useState([]);
   const slider = React.useRef(null);
+
   useEffect(() => {
     fetch("/public/menu.json")
       .then((res) => res.json())
@@ -15,15 +46,13 @@ const SpecialDishes = () => {
         setRecipes(specials);
       });
   }, []);
-
-  // settings
   const settings = {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 1,
     responsive: [
       {
         breakpoint: 1024,
@@ -35,7 +64,7 @@ const SpecialDishes = () => {
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 970,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
@@ -43,41 +72,41 @@ const SpecialDishes = () => {
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
         },
       },
     ],
+
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
   return (
-    <div>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 my-20 relative">
+       <div className='text-left'>
+            <p className='subtitle'>Customer Favorites</p>
+            <h2 className='title'>Popular Catagories</h2>
         </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
+      <div className="md:absolute right-3 top-8 mb-10 md:mr-24">
+        <button onClick={() => slider?.current?.slickPrev()}
+        className=" btn p-2 rounded-full ml-5"
+        >
+        <FaAngleLeft className=" h-8 w-8 p-1"/>
+        </button>
+        <button
+          className="bg-green btn p-2 rounded-full ml-5"
+          onClick={() => slider?.current?.slickNext()}
+        >
+          <FaAngleRight className=" h-8 w-8 p-1"/>
+        </button>
+      </div>
+
+      <Slider ref={slider} {...settings} className="overflow-hidden mt-10 space-x-5">
+        {recipes.map((item, i) => (
+          <Cards item={item} key={i}/>
+        ))}
       </Slider>
     </div>
   );
