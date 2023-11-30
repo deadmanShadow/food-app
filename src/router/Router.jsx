5eaf0344cf0aae9396ce2d3bf0350da011e86347
Abdullah Raihan Shamil <raihanshamil33@gmail.com0>
2023-11-30 20:15:06 +0600

    sign with modal added

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/home/Home";
 import Menu from "../pages/home/shop/Menu";
+import Signup from "../components/Signup";
 
 const router = createBrowserRouter([
   {
@@ -15,9 +16,13 @@ const router = createBrowserRouter([
         {
           path: "/menu",
           element: <Menu/>
-        }
-    ]
+        },
+    ],
   },
+  {
+    path: "/signup",
+    element: <Signup/>
+  }
 ]);
 
 export default router;
