6b28d125f4ce9d2c3e8cb7066b2ddb9cec1db9b6
Abdullah Raihan Shamil <raihanshamil33@gmail.com0>
2023-12-01 18:26:57 +0600

    spinn added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,17 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const Main = () => {
-    return (
-        <div>
-            <Navbar/>
-            <div className="min-h-screen">
-                <Outlet/>
-            </div>
-            <Footer/>
-        </div>
-    );
-};
+    const {loading} = useContext(AuthContext);
 
+    return (
+        <div className="bg-prigmayBG">
+          {loading ? (
+            <LoadingSpinner />
+          ) : (
+            <div>
+            <Navbar />
+            <Outlet />
+            <Footer />
+          </div>
+          )}
+           
+        </div>
+      );
+    };
 export default Main;
